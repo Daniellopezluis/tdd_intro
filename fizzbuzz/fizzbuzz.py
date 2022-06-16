5a2800a9c6ea864c437d730faae8719b020672d6
marisamstw <104351292+marisamstw@users.noreply.github.com>
2022-06-16 10:44:19 +0200

    four use cases and related tests implementation

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,5 +1,9 @@
 def fizzfuzz(number):
+    if (number % 3 == 0) and (number % 5 == 0):
+        return "fizzbuzz"   
     if number % 3 == 0:
         return "fizz"
-
-    return ""
+    if number % 5 == 0:
+        return "buzz"             
+        
+    return str(number)
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -8,10 +8,20 @@ class FizzBuzzTest(unittest.TestCase):
     def test_multiple_of_three_returns_fizz(self):
         ret = fizzbuzz.fizzfuzz(3)
         self.assertEqual("fizz", ret)
+    # add your tests here - modify however you want
+    def test_multiple_of_five_returns_buzz(self):
+        ret = fizzbuzz.fizzfuzz(5)
+        self.assertEqual("buzz", ret)        
 
-    def test_when_not_multiple_of_three_returns_empty(self):
+    # add your tests here - modify however you want
+    def test_multiple_of_three_and_five_returns_fizzbuzz(self):
+        ret = fizzbuzz.fizzfuzz(15)
+        self.assertEqual("fizzbuzz", ret)   
+
+    def test_when_not_multiple_of_three_nor_five_returns_tostring(self):
         ret = fizzbuzz.fizzfuzz(2)
-        self.assertEqual("", ret)
+        self.assertEqual(str(2), ret)
+
 
 if __name__ == '__main__':
     unittest.main()
