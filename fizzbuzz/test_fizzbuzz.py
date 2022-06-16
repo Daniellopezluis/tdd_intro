92de4bf02b0a0ab127a9dc59ae9368f853257640
Jonathan Carrillo <jonathalexander.c@thoughtworks.com>
2022-06-16 10:31:11 +0200

    creating test for 2nd requirement

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -1,9 +1,16 @@
 import unittest
 from fizzbuzz import FizzBuzz
 
+# 1. For multiples of three return “Fizz” instead of the number
+# 2. For the multiples of five return “Buzz”
+# 3. For numbers that are multiples of both three and five return “FizzBuzz”.
+
 
 class FizzBuzzTest(unittest.TestCase):
     def test_multiple_3_should_return_fizz(self):
+        self.assertEqual(FizzBuzz(3), "fizz")
+
+    def test_multiple_5_should_return_buzz(self):
         self.assertEqual(FizzBuzz(3), "fizz")
 
 
